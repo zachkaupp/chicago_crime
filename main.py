f5a02441cb94e97fef60dec0245aa19967178dc1
zachkaupp <zachkaupp@gmail.com>
2023-08-06 13:30:39 -0500

    allow visualization of data with "webbrowser"

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import pandas as pd
 import numpy as np
+import webbrowser
+import os
 
-data = pd.read_csv("data/kaggle/crimes_2001-present.csv")
-data_filtered = data.dropna(axis=0,subset=["Location"])
+df = pd.read_csv("data/kaggle/crimes_2001-present.csv")
+df = df.dropna(axis=0,subset=["Location"])
+
+filename = "htmlstyler.html"
+with open(filename, "w") as f:
+    df[0:5].style.to_html(f)
+
+abspath = os.path.abspath(filename)
+webbrowser.open(f"file:{abspath}", new = 1)
