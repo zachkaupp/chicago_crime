9a7332f4acdb90e2365302e6f731fffbfdecb159
zachkaupp <zachkaupp@gmail.com>
2023-08-05 19:08:53 -0500

    import data in main

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-# this is where the program will be written
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv("data/kaggle/crimes_2001-present.csv")
+data_filtered = data.dropna(axis=0,subset=["Location"])
