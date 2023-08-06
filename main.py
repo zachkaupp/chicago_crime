3ff443cb7735060a1de8428c0e6cc27b90b224e9
zachkaupp <zachkaupp@gmail.com>
2023-08-06 13:47:25 -0500

    prune unnecessary data

--- main.py
+++ main.py
@@ -5,10 +5,20 @@ import os
 
 df = pd.read_csv("data/kaggle/crimes_2001-present.csv")
 df = df.dropna(axis=0,subset=["Location"])
+df_prune = df.drop(["Case Number",
+                    "Block",
+                    "Primary Type",
+                    "Description",
+                    "FBI Code",
+                    "X Coordinate",
+                    "Y Coordinate",
+                    "Year",
+                    "Updated On",
+                    "Location"], axis=1)
 
 filename = "htmlstyler.html"
 with open(filename, "w") as f:
-    df[0:5].style.to_html(f)
+    df_prune[0:21].style.to_html(f)
 
 abspath = os.path.abspath(filename)
 webbrowser.open(f"file:{abspath}", new = 1)
